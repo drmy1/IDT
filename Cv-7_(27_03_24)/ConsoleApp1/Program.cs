140f51483eab918414d7ed47d983f169318a44dd
drmy1 <62697911+drmy1@users.noreply.github.com>
2024-03-27 10:39:01 +0100

    new file:   Cv-7_(27_03_24)/.vs/Cv-7_(27_03_24)/FileContentIndex/1bc34878-58b6-4ba6-9835-7d24bd04f181.vsidx 	new file:   Cv-7_(27_03_24)/.vs/Cv-7_(27_03_24)/v17/.wsuo 	new file:   Cv-7_(27_03_24)/.vs/ProjectSettings.json 	new file:   Cv-7_(27_03_24)/.vs/VSWorkspaceState.json 	new file:   Cv-7_(27_03_24)/.vs/slnx.sqlite 	new file:   Cv-7_(27_03_24)/.vscode/settings.json 	new file:   Cv-7_(27_03_24)/ConsoleApp1.sln 	new file:   Cv-7_(27_03_24)/ConsoleApp1/ConsoleApp1.csproj 	new file:   Cv-7_(27_03_24)/ConsoleApp1/Program.cs 	new file:   Cv-7_(27_03_24)/ConsoleApp1/bin/Debug/net8.0/ConsoleApp1.deps.json 	new file:   Cv-7_(27_03_24)/ConsoleApp1/bin/Debug/net8.0/ConsoleApp1.dll 	new file:   Cv-7_(27_03_24)/ConsoleApp1/bin/Debug/net8.0/ConsoleApp1.exe 	new file:   Cv-7_(27_03_24)/ConsoleApp1/bin/Debug/net8.0/ConsoleApp1.pdb 	new file:   Cv-7_(27_03_24)/ConsoleApp1/bin/Debug/net8.0/ConsoleApp1.runtimeconfig.json 	new file:   Cv-7_(27_03_24)/ConsoleApp1/obj/ConsoleApp1.csproj.nuget.dgspec.json 	new file:   Cv-7_(27_03_24)/ConsoleApp1/obj/ConsoleApp1.csproj.nuget.g.props 	new file:   Cv-7_(27_03_24)/ConsoleApp1/obj/ConsoleApp1.csproj.nuget.g.targets 	new file:   Cv-7_(27_03_24)/ConsoleApp1/obj/Debug/net8.0/.NETCoreApp,Version=v8.0.AssemblyAttributes.cs 	new file:   Cv-7_(27_03_24)/ConsoleApp1/obj/Debug/net8.0/ConsoleApp1.AssemblyInfo.cs 	new file:   Cv-7_(27_03_24)/ConsoleApp1/obj/Debug/net8.0/ConsoleApp1.AssemblyInfoInputs.cache 	new file:   Cv-7_(27_03_24)/ConsoleApp1/obj/Debug/net8.0/ConsoleApp1.GeneratedMSBuildEditorConfig.editorconfig 	new file:   Cv-7_(27_03_24)/ConsoleApp1/obj/Debug/net8.0/ConsoleApp1.GlobalUsings.g.cs 	new file:   Cv-7_(27_03_24)/ConsoleApp1/obj/Debug/net8.0/ConsoleApp1.assets.cache 	new file:   Cv-7_(27_03_24)/ConsoleApp1/obj/Debug/net8.0/ConsoleApp1.csproj.CoreCompileInputs.cache 	new file:   Cv-7_(27_03_24)/ConsoleApp1/obj/Debug/net8.0/ConsoleApp1.csproj.FileListAbsolute.txt 	new file:   Cv-7_(27_03_24)/ConsoleApp1/obj/Debug/net8.0/ConsoleApp1.dll 	new file:   Cv-7_(27_03_24)/ConsoleApp1/obj/Debug/net8.0/ConsoleApp1.genruntimeconfig.cache 	new file:   Cv-7_(27_03_24)/ConsoleApp1/obj/Debug/net8.0/ConsoleApp1.pdb 	new file:   Cv-7_(27_03_24)/ConsoleApp1/obj/Debug/net8.0/ConsoleApp1.sourcelink.json 	new file:   Cv-7_(27_03_24)/ConsoleApp1/obj/Debug/net8.0/apphost.exe 	new file:   Cv-7_(27_03_24)/ConsoleApp1/obj/Debug/net8.0/ref/ConsoleApp1.dll 	new file:   Cv-7_(27_03_24)/ConsoleApp1/obj/Debug/net8.0/refint/ConsoleApp1.dll 	new file:   Cv-7_(27_03_24)/ConsoleApp1/obj/Release/net8.0/.NETCoreApp,Version=v8.0.AssemblyAttributes.cs 	new file:   Cv-7_(27_03_24)/ConsoleApp1/obj/Release/net8.0/ConsoleApp1.AssemblyInfo.cs 	new file:   Cv-7_(27_03_24)/ConsoleApp1/obj/Release/net8.0/ConsoleApp1.AssemblyInfoInputs.cache 	new file:   Cv-7_(27_03_24)/ConsoleApp1/obj/Release/net8.0/ConsoleApp1.GeneratedMSBuildEditorConfig.editorconfig 	new file:   Cv-7_(27_03_24)/ConsoleApp1/obj/Release/net8.0/ConsoleApp1.GlobalUsings.g.cs 	new file:   Cv-7_(27_03_24)/ConsoleApp1/obj/Release/net8.0/ConsoleApp1.assets.cache 	new file:   Cv-7_(27_03_24)/ConsoleApp1/obj/project.assets.json 	new file:   Cv-7_(27_03_24)/ConsoleApp1/obj/project.nuget.cache

---
+++ Cv-7_(27_03_24)/ConsoleApp1/Program.cs
@@ -0,0 +1,82 @@
+﻿/**
+ * Prichozi hovor do call centra
+ */
+public class IncomingCall
+{
+	/** Volajici cislo */
+	int callingNumber;
+	/** Cas kdy hovor prisel (v sekundach od zacatku smeny) */
+	int time;
+}
+
+/**
+ * Prvek spojoveho seznamu
+ */
+public class Link
+{
+	/** Data prvku - prichozi hovor do call centra */
+	public IncomingCall data;
+	/** Dalsi prvek spojoveho seznamu */
+	public Link next;
+}
+
+/**
+ * Fronta prichozich hovoru
+ */
+public class CallerQueue
+{
+	/** Prvni prvek fronty */
+	private Link first;
+	/** Posledni prvek fronty */
+	private Link last;
+
+	/**
+	 * Prida prichozi hovor na konec fronty
+	 * @param call prichozi hovor
+	 */
+	public void Add(IncomingCall call)
+	{
+		Link nl = new Link();
+		nl.data = call;
+		if (first == null)
+		{
+			first = nl;
+			last = nl;
+		}
+		else
+		{
+			last = nl;
+		}
+	}
+
+	/**
+	 * Vrati prvni prichozi hovor nebo null, pokud je fronta prazdna
+	 * @return prvni prichozi hovor nebo null, pokud je fronta prazdna
+	 */
+	public IncomingCall Get()
+	{
+		if (first != null)
+		{
+			return first.data;
+		}
+		else
+		{
+			return null;
+		}
+	}
+
+	/**
+	 * Odstrani prvni prichozi hovor z fronty, pokud fronta neni prazdna
+	 */
+	public void RemoveFirst()
+	{
+		if (first != null)
+		{
+			first = first.next;
+		}
+		else
+		{
+			Console.WriteLine("Remove call on empty queue. Probably error, continuing...");
+		}
+	}
+}
