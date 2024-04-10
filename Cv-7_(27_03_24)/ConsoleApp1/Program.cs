bf91042db9ead23bb05d2e8970051fdc81d3a1ce
drmy1 <62697911+drmy1@users.noreply.github.com>
2024-04-10 11:20:18 +0200

    modified:   Cv-7_(27_03_24)/ConsoleApp1/Program.cs 	modified:   Cv-7_(27_03_24)/ConsoleApp1/obj/Debug/net8.0/ConsoleApp1.AssemblyInfo.cs 	modified:   Cv-7_(27_03_24)/ConsoleApp1/obj/Debug/net8.0/ConsoleApp1.AssemblyInfoInputs.cache 	modified:   Cv-7_(27_03_24)/ConsoleApp1/obj/Debug/net8.0/ConsoleApp1.sourcelink.json 	new file:   Cv-9_(10_04_24)/.vscode/settings.json 	new file:   Cv-9_(10_04_24)/ConsoleApp1.sln 	new file:   Cv-9_(10_04_24)/ConsoleApp1/ConsoleApp1.csproj 	new file:   Cv-9_(10_04_24)/ConsoleApp1/Program.cs 	new file:   Cv-9_(10_04_24)/ConsoleApp1/bin/Debug/net8.0/ConsoleApp1.deps.json 	new file:   Cv-9_(10_04_24)/ConsoleApp1/bin/Debug/net8.0/ConsoleApp1.dll 	new file:   Cv-9_(10_04_24)/ConsoleApp1/bin/Debug/net8.0/ConsoleApp1.exe 	new file:   Cv-9_(10_04_24)/ConsoleApp1/bin/Debug/net8.0/ConsoleApp1.pdb 	new file:   Cv-9_(10_04_24)/ConsoleApp1/bin/Debug/net8.0/ConsoleApp1.runtimeconfig.json 	new file:   Cv-9_(10_04_24)/ConsoleApp1/obj/ConsoleApp1.csproj.nuget.dgspec.json 	new file:   Cv-9_(10_04_24)/ConsoleApp1/obj/ConsoleApp1.csproj.nuget.g.props 	new file:   Cv-9_(10_04_24)/ConsoleApp1/obj/ConsoleApp1.csproj.nuget.g.targets 	new file:   Cv-9_(10_04_24)/ConsoleApp1/obj/Debug/net8.0/.NETCoreApp,Version=v8.0.AssemblyAttributes.cs 	new file:   Cv-9_(10_04_24)/ConsoleApp1/obj/Debug/net8.0/ConsoleApp1.AssemblyInfo.cs 	new file:   Cv-9_(10_04_24)/ConsoleApp1/obj/Debug/net8.0/ConsoleApp1.AssemblyInfoInputs.cache 	new file:   Cv-9_(10_04_24)/ConsoleApp1/obj/Debug/net8.0/ConsoleApp1.GeneratedMSBuildEditorConfig.editorconfig 	new file:   Cv-9_(10_04_24)/ConsoleApp1/obj/Debug/net8.0/ConsoleApp1.GlobalUsings.g.cs 	new file:   Cv-9_(10_04_24)/ConsoleApp1/obj/Debug/net8.0/ConsoleApp1.assets.cache 	new file:   Cv-9_(10_04_24)/ConsoleApp1/obj/Debug/net8.0/ConsoleApp1.csproj.CoreCompileInputs.cache 	new file:   Cv-9_(10_04_24)/ConsoleApp1/obj/Debug/net8.0/ConsoleApp1.csproj.FileListAbsolute.txt 	new file:   Cv-9_(10_04_24)/ConsoleApp1/obj/Debug/net8.0/ConsoleApp1.dll 	new file:   Cv-9_(10_04_24)/ConsoleApp1/obj/Debug/net8.0/ConsoleApp1.genruntimeconfig.cache 	new file:   Cv-9_(10_04_24)/ConsoleApp1/obj/Debug/net8.0/ConsoleApp1.pdb 	new file:   Cv-9_(10_04_24)/ConsoleApp1/obj/Debug/net8.0/ConsoleApp1.sourcelink.json 	new file:   Cv-9_(10_04_24)/ConsoleApp1/obj/Debug/net8.0/apphost.exe 	new file:   Cv-9_(10_04_24)/ConsoleApp1/obj/Debug/net8.0/ref/ConsoleApp1.dll 	new file:   Cv-9_(10_04_24)/ConsoleApp1/obj/Debug/net8.0/refint/ConsoleApp1.dll 	new file:   Cv-9_(10_04_24)/ConsoleApp1/obj/project.assets.json 	new file:   Cv-9_(10_04_24)/ConsoleApp1/obj/project.nuget.cache

--- Cv-7_(27_03_24)/ConsoleApp1/Program.cs
+++ Cv-7_(27_03_24)/ConsoleApp1/Program.cs
@@ -4,9 +4,9 @@
 public class IncomingCall
 {
 	/** Volajici cislo */
-	int callingNumber;
+	public int callingNumber;
 	/** Cas kdy hovor prisel (v sekundach od zacatku smeny) */
-	int time;
+	public int time;
 }
 
 /**
@@ -45,6 +45,7 @@ public class CallerQueue
 		}
 		else
 		{
+			last.next = nl;
 			last = nl;
 		}
 	}
@@ -78,5 +79,82 @@ public class CallerQueue
 		{
 			Console.WriteLine("Remove call on empty queue. Probably error, continuing...");
 		}
+	}
+}
+
+/**
+ * Odbavovani prichozich hovoru pomoci operatoru
+ */
+class Dispatcher {
+	/** Fronta prichozich hovoru */
+	private CallerQueue callerQueue;
+	/** Fronta operatoru */
+	private OperatorQueue operatorQueue;
+	
+	/**
+	 * Vytvori novou instanci s prazdnymi frontami
+	 */
+	public Dispatcher() {
+		this.callerQueue = new CallerQueue();
+		this.operatorQueue = new OperatorQueue();
+	}
+	
+	/**
+	 * Zaradi prichozi hovor do fronty
+	 * @param number telefonni cislo prichoziho hvoru
+	 * @param time cas zacatku hovoru (v sekundach od zacatku smeny)
+	 */
+	public void Call(int number, int time) {
+		IncomingCall call = new IncomingCall();
+		call.callingNumber = number;
+		call.time = time;
+		callerQueue.Add(call);
+	}
+	
+	/**
+	 * Zaradi volneho operatora do fronty
+	 * @param name jmeno volneho operatora
+	 * @param time cas zarazeni volneho operatora do fronty (v sekundach od zacatku smeny)
+	 */
+	public void FreeOperator(String name, int time) {
+		operatorQueue.Add(new FreeOperator(name, time)); // operator name se time sekund od zacatku smeny prihlasil jako dostupny
+	}
+
+	/**
+	 * Priradi nejdele cekajici hovor z fronty nejdele cekajicimu operatorovi z fronty
+	 */
+	public void DispatchCall() {
+	}
+	
+	/**
+	 * Priradi zadany prichozi hovor zadanemu volnemu operatorovi 
+	 * @param call prichozi hovor
+	 * @param operator volny operator
+	 */
+	private void AssignCall(IncomingCall call, FreeOperator operator) {
+		Console.WriteLine(operator.name + " is answering call from +420 " + call.callingNumber);
+		Console.WriteLine("The caller has waited for " + Math.max(0, operator.time - call.time) + " seconds.");
+	}
+}
+
+public class CallDispatching {
+	public static void Main(String[] args) {
+		Dispatcher d = new Dispatcher();
+		d.FreeOperator("Tonda", 0);
+		d.DispatchCall();
+		d.FreeOperator("Jarmila", 10);
+		d.DispatchCall();
+		d.Call(608123456, 15);
+		d.DispatchCall();
+		d.Call(723987654, 35);
+		d.DispatchCall();
+		d.Call(602112233, 45);
+		d.DispatchCall();
+		d.FreeOperator("Pepa", 62);
+		d.DispatchCall();
+		d.Call(608987654, 124);
+		d.DispatchCall();
+		d.FreeOperator("Tonda", 240);
+		d.DispatchCall();
 	}
 }
